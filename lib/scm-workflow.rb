91dfc0ccbd2b49daca2b576a168893fb75376b93
Pat Laplante <pat@covenofchaos.com>
2013-02-04 15:11:35 -0500

    wip

---
+++ lib/scm-workflow.rb
@@ -0,0 +1,7 @@
+require "scm-workflow/version"
+
+module Scm
+  module Workflow
+    # Your code goes here...
+  end
+end
---
+++ lib/scm-workflow/version.rb
@@ -0,0 +1,5 @@
+module Scm
+  module Workflow
+    VERSION = "0.0.1"
+  end
+end
